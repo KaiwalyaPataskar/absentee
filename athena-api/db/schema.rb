0012ee6677091fa32fb7718d2fd95ab591dd892a
Kaiwalya Pataskar <kaiwalya.pataskar@joshsoftware.com>
2019-04-26 19:52:30 +0530

    db_setup

--- athena-api/db/schema.rb
+++ athena-api/db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_140910) do
+ActiveRecord::Schema.define(version: 2019_04_26_142026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "class_infos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "divisions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schools", force: :cascade do |t|
     t.string "name"
