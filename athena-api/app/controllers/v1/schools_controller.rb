2d63edba1ea60bc9fe33fa08a424c9942ce10385
smriti gupta <smriti.gupta@joshsoftware.com>
2019-04-26 22:51:45 +0530

    Added School CRUD

---
+++ athena-api/app/controllers/v1/schools_controller.rb
@@ -0,0 +1,42 @@
+class V1::SchoolsController < ApplicationController
+  before_action :set_school, only: [:show, :update, :destroy]
+  
+  def index
+    @schools = School.all
+    json_response(@users)
+  end
+
+  # POST v1/schools
+  def create
+    @school = School.create!(school_params)
+    json_response(@school, :created)
+  end
+
+  # GET v1/schools/:id
+  def show
+    json_response(@school)
+  end
+
+  # PUT v1/schools/:id
+  def update
+    @school.update(school_params)
+    json_response(@school, :updated)
+  end
+
+  # DELETE v1/users/:id
+  def destroy
+    @school.destroy
+    json_response(@school, :destroyed)
+  end
+
+  private
+
+  def school_params
+    # whitelist params
+    params.permit(:name, :address, :contact_number)
+  end
+
+  def set_user
+    @school = School.find(params[:id])
+  end
+end
--- athena-api/config/routes.rb
+++ athena-api/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   end
   api_version(:module => "V1", :path => {:value => "v1"}) do
   	resources :users
+  	resources :schools
   end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
