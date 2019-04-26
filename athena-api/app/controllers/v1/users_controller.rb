4e985c8ab5c1dc780bca17acc01baf3e87b596b8
smriti gupta <smriti.gupta@joshsoftware.com>
2019-04-26 20:16:22 +0530

    Added some common files for response.

---
+++ athena-api/app/controllers/v1/users_controller.rb
@@ -0,0 +1,41 @@
+class V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  # GET /todos/:id
+  def show
+    json_response(@todo)
+  end
+
+  # PUT /todos/:id
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+end
